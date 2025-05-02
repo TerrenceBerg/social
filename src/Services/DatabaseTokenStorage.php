460884f57207bd4124134269523846fbe005e00f
Sanaullah Kiani <kianisanaullah@gmail.com>
2025-05-02 22:30:04 +0500

    Update DatabaseTokenStorage.php

--- src/Services/DatabaseTokenStorage.php
+++ src/Services/DatabaseTokenStorage.php
@@ -51,15 +51,19 @@ class DatabaseTokenStorage implements TokenStorageInterface
 
             $token->access_token = $tokenData['access_token'] ?? $token->access_token;
             $token->refresh_token = $tokenData['refresh_token'] ?? $token->refresh_token;
-
             if (isset($tokenData['expires_in'])) {
                 $token->expires_at = Carbon::now()->addSeconds((int)$tokenData['expires_in']);
             } elseif (isset($tokenData['expires_at'])) {
                 $timestamp = $tokenData['expires_at'];
+
                 if (is_numeric($timestamp)) {
                     $token->expires_at = Carbon::createFromTimestamp((int)$timestamp);
                 } else {
-                    throw new \Exception("Invalid 'expires_at' format: $timestamp");
+                    try {
+                        $token->expires_at = Carbon::parse($timestamp);
+                    } catch (\Exception $ex) {
+                        throw new \Exception("Invalid 'expires_at' format: $timestamp");
+                    }
                 }
             }
 
